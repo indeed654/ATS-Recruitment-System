c873ea73c8b24f12093d5151d78b86a9668476fb
YASH KUMAR SHARMA <yash1047sharma@gmail.com>
2026-07-10 22:01:26 +0530

    Fix quote style for apiUrl in environment.prod.ts

--- ATDRIVEATS/atdrive-ats/src/environments/environment.prod.ts
+++ ATDRIVEATS/atdrive-ats/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://ats-recruitment-system-o6uo.onrender.com/api"'
+  apiUrl: 'https://ats-recruitment-system-o6uo.onrender.com/api'
 };
