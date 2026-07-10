18ebbad980682f0fccadc10437cab1a92b09ea41
YASH KUMAR SHARMA <yash1047sharma@gmail.com>
2026-07-10 21:59:43 +0530

    Fix apiUrl syntax in environment.prod.ts

--- ATDRIVEATS/atdrive-ats/src/environments/environment.prod.ts
+++ ATDRIVEATS/atdrive-ats/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'hapiUrl: "https://ats-recruitment-system-o6uo.onrender.com/api"'
+  apiUrl: "https://ats-recruitment-system-o6uo.onrender.com/api"'
 };
